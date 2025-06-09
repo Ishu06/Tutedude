2bc6774eee55bf8dde0dc04a31ab7fffe14eadc1
Ishu06 <ishukul300@gmail.com>
2025-06-09 20:19:23 +0530

    Delete Task 2.py

--- Task 2.py
+++
@@ -1,8 +0,0 @@
-#Assignment_1 from python course
-#Task2 - Create a personalised Greeting.
-
-a = input('Enter your first name: ')
-b = input('Enter your last name: ')
-fn = a + " " + b
-print(f"Hello, {fn}! Welcome to the Python program.")
-
